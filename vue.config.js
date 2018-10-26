91ea994d849c8454dd5c95eff557560b9b75a2b5
zenghongtu <zenghongtu@gmail.com>
2018-10-26 12:19:40 +0800

    chore: update vue.config

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,28 @@
+const path = require('path');
+
 module.exports = {
   lintOnSave: false,
+  chainWebpack: (config) => {
+    config
+      .entry('app')
+      .clear();
+    config
+      .entry('app')
+      .add('./examples/main.js')
+      .end()
+      .module
+      .rule('md')
+      .test(/\.md/)
+      .use('vue-loader')
+      .loader('vue-loader')
+      .end()
+      .use('vue-markdown-loader')
+      .loader('vue-markdown-loader/lib/markdown-compiler')
+      .options({
+        raw: true,
+      })
+      .end();
+    config.resolve.alias
+      .set('@', path.join(__dirname, 'examples'));
+  },
 };
