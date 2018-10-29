28a53ae523e08df3c240b385654d84e95518c20f
zenghongtu <zenghongtu@gmail.com>
2018-10-29 20:23:27 +0800

    refactor: update side-nav

--- examples/components/side-nav.vue
+++ examples/components/side-nav.vue
@@ -5,30 +5,26 @@
 
 <template>
   <div class="side-nav">
-    <div v-for="title in (Object.keys(data))">
-      <p class="side-nav-title">{{title}}</p>
-      <div class="side-nav-items" v-for="nav in data[title]">
-        <router-link :class="$route.name===nav.name ? 'active' : ''" v-if="nav.name" :to="{name: nav.name}">
-          {{nav.desc}}
+    <div v-for="nav in navs" :key="nav.name">
+      <p class="side-nav-title">{{nav.name}}</p>
+      <div class="side-nav-items" v-for="page in nav['groups']" :key="page.name">
+        <router-link :class="$route.name===page.name ? 'active' : ''" v-if="page.name"
+                     :to="{name: page.name}">
+          {{page.description}}
         </router-link>
-        <p v-else class="side-nav-group">{{nav.desc}}</p>
-        <div v-for="item in nav.items">
-          <router-link :to="{name: item.name}" :class="$route.name===item.name ? 'active' : ''"
-                       class="slid-nav-component">{{item.desc}}
-          </router-link>
-        </div>
+        <p v-else class="side-nav-group">{{page.description}}</p>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import navConf from '../nav.config.json';
+import navConfig from '../nav.config.json';
 
 export default {
   data() {
     return {
-      data: navConf,
+      navs: navConfig.documentation,
     };
   },
 };
