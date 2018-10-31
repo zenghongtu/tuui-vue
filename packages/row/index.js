b8e0e1a78e9575390ddb9d822526cb98f5533623
zenghongtu <zenghongtu@gmail.com>
2018-10-31 18:29:43 +0800

    feat: add basic layout

---
+++ packages/row/index.js
@@ -0,0 +1,13 @@
+/**
+ * Created by zenghongtu on 31/10/2018.
+ * Desc: index
+ */
+
+import Row from './src/row';
+
+Row.install = function (Vue) {
+  Vue.component(Row.name, Row);
+};
+
+export default Row;
+
