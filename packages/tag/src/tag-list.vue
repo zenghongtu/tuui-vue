afa77ff530f6d5c20b358dca2d0d48c73880287e
zenghongtu <zenghongtu@gmail.com>
2018-11-21 18:26:45 +0800

    feat: add tag-list component

---
+++ packages/tag/src/tag-list.vue
@@ -0,0 +1,20 @@
+/**
+* Created by zenghongtu on 21/11/2018.
+* Desc: main.vue
+*/
+
+<template>
+  <div class="tags"
+       :class="{'has-addons':addon}">
+    <slot></slot>
+  </div>
+</template>
+<script>
+export default {
+  name: 'TuTagList',
+  props: {
+    addon: Boolean,
+  },
+};
+</script>
+
