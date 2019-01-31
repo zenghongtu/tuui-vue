32e69a97bb81ee5f125b0460ac99810de62a869c
zenghongtu <zenghongtu@gmail.com>
2019-01-31 16:35:58 +0800

    Updates

---
+++ js/chunk-2d0d8389.126a6929.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0d8389"],{"79c4":function(n,e,l){"use strict";l.r(e);var t=function(){var n=this,e=n.$createElement,l=n._self._c||e;return l("section",[l("tu-box",[n._v("\n    hello\n")])],1)},u=[],c=l("2455"),o=null,s=!1,r=null,a=null,i=null,p=Object(c["a"])(o,t,u,s,r,a,i);e["default"]=p.exports}}]);
+//# sourceMappingURL=chunk-2d0d8389.126a6929.js.map
